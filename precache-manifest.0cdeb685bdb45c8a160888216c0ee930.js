59d6d3868ed1a5c4736c7510ac9752fe9bbd2232
Yego Allan <allanyego05@gmail.com>
2020-07-17 08:38:25 +0000

    Updates

---
+++ precache-manifest.0cdeb685bdb45c8a160888216c0ee930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "074d24ca0e5c6d30f46e0e178833c7a8",
+    "url": "/paypal_proto/index.html"
+  },
+  {
+    "revision": "d9dc526cf65e1654e1b2",
+    "url": "/paypal_proto/static/css/2.8ddda0ad.chunk.css"
+  },
+  {
+    "revision": "d9dc526cf65e1654e1b2",
+    "url": "/paypal_proto/static/js/2.50ec8351.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/paypal_proto/static/js/2.50ec8351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0fd64e0c63cdc08dd84",
+    "url": "/paypal_proto/static/js/main.8c246e80.chunk.js"
+  },
+  {
+    "revision": "da3aae1055c58f220d9f",
+    "url": "/paypal_proto/static/js/runtime-main.3ed46e34.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c98118742fe44fcc28bc283f6c9df692.js"
+  "/paypal_proto/precache-manifest.0cdeb685bdb45c8a160888216c0ee930.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/paypal_proto/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
