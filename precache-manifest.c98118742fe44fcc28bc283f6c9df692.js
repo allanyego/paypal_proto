6322d6ad8a922e854cf375c95471874fedee0c55
Yego Allan <allanyego05@gmail.com>
2020-07-17 08:35:54 +0000

    Updates

---
+++ precache-manifest.c98118742fe44fcc28bc283f6c9df692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf351a7f583bc9a952a2a7ad9c733823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9dc526cf65e1654e1b2",
+    "url": "/static/css/2.8ddda0ad.chunk.css"
+  },
+  {
+    "revision": "d9dc526cf65e1654e1b2",
+    "url": "/static/js/2.50ec8351.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.50ec8351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0fd64e0c63cdc08dd84",
+    "url": "/static/js/main.8c246e80.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  }
+]);
